00d37adb314ff6544e1589f734da2365d78d4645
kirtidahake <kirtidahake955@gmail.com>
2025-04-28 23:05:39 +0530

    23,24,25

--- Circle App/Controllers/HomeController.cs
+++ Circle App/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
+using Circle_App.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace Circle_App.Controllers
@@ -6,15 +8,18 @@ namespace Circle_App.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var allPosts = await _context.Posts.Include(u => u.User).ToListAsync();
+            return View(allPosts);
         }
 
         
