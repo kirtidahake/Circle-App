f4292b0035d210661df7e957690b9f4a3326dd90
kirtidahake <kirtidahake955@gmail.com>
2025-05-01 00:24:41 +0530

    29

--- Circle App/Controllers/HomeController.cs
+++ Circle App/Controllers/HomeController.cs
@@ -41,6 +41,24 @@ namespace Circle_App.Controllers
                 NrOfReports = 0,
                 UserId = loggedInUser
             };
+
+            if (post.Image != null && post.Image.Length > 0)
+            {
+                string rootFolderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
+                if (post.Image.ContentType.Contains("image"))
+                {
+                    string rooFolderPathImages = Path.Combine(rootFolderPath, "images");
+                    Directory.CreateDirectory(rooFolderPathImages);
+
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(post.Image.FileName);
+                    string filePath = Path.Combine(rooFolderPathImages, fileName);
+
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                        await post.Image.CopyToAsync(stream);
+
+                    newPost.ImageUrl = "/images/" + fileName;
+                }
+            }
             await _context.Posts.AddAsync(newPost);
             await _context.SaveChangesAsync();
 
