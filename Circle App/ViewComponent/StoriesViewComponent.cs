bdb3418c4900a6e1efc344bd5cfadc845275ff0b
kirtidahake <kirtidahake955@gmail.com>
2025-05-05 23:42:37 +0530

    65

---
+++ Circle App/ViewComponent/StoriesViewComponent.cs
@@ -0,0 +1,22 @@
+﻿
+using Circle_App.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+namespace Circle_App.ViewComponents
+{
+    public class StoriesViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StoriesViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var allStories = _context.Stories.Include(u => u.User).ToList();
+            return View(allStories);
+        }
+
+    }
+}
