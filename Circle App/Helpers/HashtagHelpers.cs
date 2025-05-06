e58d1d12a1394f74e73c99037842791849e41a66
kirtidahake <kirtidahake955@gmail.com>
2025-05-07 00:11:39 +0530

    70

---
+++ Circle App/Helpers/HashtagHelpers.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Circle_App.Helpers
+{
+    public static class HashtagHelpers
+    {
+        public static List<string> GetHashtage(string postContent)
+        {
+            var hashtagPattern = new Regex(@"#\w+");
+            var matches = hashtagPattern.Matches(postContent)
+                .Select(matches => matches.Value.TrimEnd(',', ',', '!', '?'))
+                .Distinct()
+                .ToList();
+            return matches;
+        }
+    }
+}
