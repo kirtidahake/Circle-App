0f07ecbe928068f8fe58eff52ac3ab2470be3388
kirtidahake <kirtidahake955@gmail.com>
2025-05-07 01:49:26 +0530

    72

--- Circle App/ViewComponent/HashtagsViewComponent.cs
+++ Circle App/ViewComponent/HashtagsViewComponent.cs
@@ -1,12 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Circle_App.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Circle_App.ViewComponents
 {
     public class HashtagsViewComponent: ViewComponent
     {
+        private readonly ApplicationDbContext _context;
+
+        public HashtagsViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            return View();
+            var oneWeekAgo = DateTime.UtcNow.AddDays(-7);
+
+            var topThreeHashtags = _context.Hashtag
+                .Where(h => h.DateCreated >= oneWeekAgo)
+                .OrderByDescending(h => h.DateCreated)
+                .Take(5)
+                .ToList();
+            return View(topThreeHashtags);
         }
     }
 }
