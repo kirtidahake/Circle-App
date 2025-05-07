5787915b92355c36d8b7f7578217612537fb7945
kirtidahake <kirtidahake955@gmail.com>
2025-05-07 12:43:26 +0530

    74-75

---
+++ Circle App/Services/IPostService.cs
@@ -0,0 +1,16 @@
+﻿using Circle_App.Data.Models;
+
+namespace Circle_App.Services
+{
+    public interface IPostService
+    {
+        Task<List<Posts>> GetAllPostsAsync(int loggedInUserId);
+        Task<List<Posts>> CreatePostAsync(Posts post,IFormFile Image);
+        Task RemovePostAsync(int postId);
+        Task AddPostCommentAsync(Comment comment);
+        Task RemovePostCommentAsync(int commentId);
+        Task TogglePostLiekAsync (int postId, int userId);
+        Task TogglePostFavouriteAsync (int postId, int userId);
+        Task TogglePostVisibilityAsync (int postId, int userId);
+    }
+}
---
+++ Circle App/Services/PostService.cs
@@ -0,0 +1,53 @@
+﻿using Circle_App.Data;
+using Circle_App.Data.Models;
+
+namespace Circle_App.Services
+{
+    public class PostService : IPostService
+    {
+        private readonly ApplicationDbContext _context;
+        public PostService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        Task IPostService.AddPostCommentAsync(Comment comment)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<List<Posts>> IPostService.CreatePostAsync(Posts post, IFormFile Image)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<List<Posts>> IPostService.GetAllPostsAsync(int loggedInUserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task IPostService.RemovePostAsync(int postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task IPostService.RemovePostCommentAsync(int commentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task IPostService.TogglePostFavouriteAsync(int postId, int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task IPostService.TogglePostLiekAsync(int postId, int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task IPostService.TogglePostVisibilityAsync(int postId, int userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
