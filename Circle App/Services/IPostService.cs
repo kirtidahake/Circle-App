cd5520d619779fda760d7555b888202d94fd4d61
kirtidahake <kirtidahake955@gmail.com>
2025-05-07 14:14:53 +0530

    76

--- Circle App/Services/IPostService.cs
+++ Circle App/Services/IPostService.cs
@@ -5,12 +5,15 @@ namespace Circle_App.Services
     public interface IPostService
     {
         Task<List<Posts>> GetAllPostsAsync(int loggedInUserId);
-        Task<List<Posts>> CreatePostAsync(Posts post,IFormFile Image);
+        Task<Posts> CreatePostAsync(Posts post,IFormFile image);
         Task RemovePostAsync(int postId);
+
         Task AddPostCommentAsync(Comment comment);
         Task RemovePostCommentAsync(int commentId);
+
         Task TogglePostLiekAsync (int postId, int userId);
         Task TogglePostFavouriteAsync (int postId, int userId);
         Task TogglePostVisibilityAsync (int postId, int userId);
+        Task ReportPostAsync(int postId, int userId);
     }
 }
--- Circle App/Services/PostService.cs
+++ Circle App/Services/PostService.cs
@@ -1,5 +1,6 @@
 ﻿using Circle_App.Data;
 using Circle_App.Data.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Circle_App.Services
 {
@@ -10,39 +11,130 @@ namespace Circle_App.Services
         {
             _context = context;
         }
-        Task IPostService.AddPostCommentAsync(Comment comment)
+
+        public async Task<List<Posts>> GetAllPostsAsync(int loggedInUserId)
         {
+            var allPosts = await _context.Posts
+                .Where(u => (!u.IsPrivate || u.UserId == loggedInUserId) && u.Reports.Count < 5 && !u.IsDeleted)
+                .Include(u => u.User)
+                .Include(u => u.Likes)
+                .Include(u => u.Favourites)
+                .Include(u => u.Reports)
+                .Include(u => u.Comments).ThenInclude(u => u.User)
+                .OrderByDescending(n => n.DateCreated)
+                .ToListAsync();
+            return allPosts;
+        }
+  
+        public async Task<Posts> CreatePostAsync(Posts post, IFormFile image)
+        {
+            if (image != null && image.Length > 0)
+            {
+                string rootFolderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
+                if (image.ContentType.Contains("image"))
+                {
+                    string rooFolderPathImages = Path.Combine(rootFolderPath, "images/posts");
+                    Directory.CreateDirectory(rooFolderPathImages);
+
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(image.FileName);
+                    string filePath = Path.Combine(rooFolderPathImages, fileName);
+
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                        await image.CopyToAsync(stream);
+
+                    post.ImageUrl = "/images/posts/" + fileName;
+                }
+            }
+
+            await _context.Posts.AddAsync(post);
+            await _context.SaveChangesAsync();
+
+            return post;
+        }
+        
+        public async Task RemovePostAsync(int postId)
+        {
+           var postDb = await _context.Posts.FirstOrDefaultAsync(p => p.PostId == postId);
+
+            if (postDb != null)
+            { 
+                postDb.IsDeleted = true;
+                _context.Posts.Update(postDb);
+                await _context.SaveChangesAsync();
+            }
+        }
+        public async Task AddPostCommentAsync(Comment comment)
+        {
+            await _context.Comments.AddAsync(comment);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task RemovePostCommentAsync(int commentId)
+        {
+            var commentDb = _context.Comments.FirstOrDefault(c => c.CommentId == commentId);
+            if (commentDb != null)
+            {
+                _context.Comments.Remove(commentDb);
+                await _context.SaveChangesAsync();
+            }
             throw new NotImplementedException();
         }
 
-        Task<List<Posts>> IPostService.CreatePostAsync(Posts post, IFormFile Image)
+        public async Task ReportPostAsync(int postId, int userId)
         {
-            throw new NotImplementedException();
+            var newReport = new Report()
+            {
+                PostId = postId,
+                UserId = userId,
+                DateCreated = DateTime.UtcNow
+            };
+            _context.Reports.Add(newReport);
+            await _context.SaveChangesAsync();
+        }
+        public async Task TogglePostFavouriteAsync(int postId, int userId)
+        {
+            var favourite = await _context.Favourites
+                 .Where(l => l.UserId == userId && l.PostId == postId)
+                 .FirstOrDefaultAsync();
+
+            if (favourite != null)
+            {
+                _context.Favourites.Remove(favourite);
+                await _context.SaveChangesAsync();
+            }
+            else
+            {
+                var newFavourite = new Favourites()
+                {
+                    PostId = postId,
+                    UserId = userId
+                };
+                _context.Favourites.Add(newFavourite);
+                await _context.SaveChangesAsync();
+            }
         }
 
-        Task<List<Posts>> IPostService.GetAllPostsAsync(int loggedInUserId)
+        public async Task TogglePostLiekAsync(int postId, int userId)
         {
-            throw new NotImplementedException();
-        }
+            var like = await _context.Likes
+                 .Where(l => l.UserId == userId && l.PostId == postId)
+                 .FirstOrDefaultAsync();
 
-        Task IPostService.RemovePostAsync(int postId)
-        {
-            throw new NotImplementedException();
-        }
-
-        Task IPostService.RemovePostCommentAsync(int commentId)
-        {
-            throw new NotImplementedException();
-        }
-
-        Task IPostService.TogglePostFavouriteAsync(int postId, int userId)
-        {
-            throw new NotImplementedException();
-        }
-
-        Task IPostService.TogglePostLiekAsync(int postId, int userId)
-        {
-            throw new NotImplementedException();
+            if (like != null)
+            {
+                _context.Likes.Remove(like);
+                await _context.SaveChangesAsync();
+            }
+            else
+            {
+                var newLike = new Likes()
+                {
+                    PostId = postId,
+                    UserId = userId
+                };
+                _context.Likes.Add(newLike);
+                await _context.SaveChangesAsync();
+            }
         }
 
         Task IPostService.TogglePostVisibilityAsync(int postId, int userId)
