7d6826d72b987c76c0eddcc1b1d273c0e7af846d
kirtidahake <kirtidahake955@gmail.com>
2025-05-07 22:35:59 +0530

    80

---
+++ Circle App/Services/FilesService.cs
@@ -0,0 +1,18 @@
+﻿
+using Circle_App.Data;
+
+namespace Circle_App.Services
+{
+    public class FilesService : IFilesService
+    {
+        private readonly ApplicationDbContext _context;
+        public FilesService(ApplicationDbContext context)
+        {
+            _context = context; 
+        }
+        public Task<string> UploadImageAsync(IFormFile file, string fileType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Circle App/Services/IFilesService.cs
@@ -0,0 +1,7 @@
+﻿namespace Circle_App.Services
+{
+    public interface IFilesService
+    {
+        Task<string> UploadImageAsync(IFormFile file,string fileType);
+    }
+}
